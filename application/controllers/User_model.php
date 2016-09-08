0c0962d62a2da6e2d835d13971b72b91c4f1a4bf
AnAQuin Better <l_dn_near@hotmail.com>
2016-09-08 09:24:31 +0000

    acm

---
+++ application/controllers/User_model.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class User_model extends CI_Model {
+    
+    protected $table_name;
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->table_name = 'tbl_users';
+    }
+    
+    public function getAll() {
+        return $this->db->from($this->table_name)->get()->result_array();
+    }
+ 
+}
+ 
+/* End of file user_model.php */
+/* Location: ./application/controllers/user_model.php */
